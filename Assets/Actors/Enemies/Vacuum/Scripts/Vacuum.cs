6a34a7a53f65be2b9ee3d56b0b22ac2af6fd4a8b
frost90909090 <frost90909090@gmail.com>
2021-02-25 16:48:06 -0300

    Comecei a fazer luta da Dispensa

---
+++ Assets/Actors/Enemies/Vacuum/Scripts/Vacuum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vacuum : MonoBehaviour{
+    Rigidbody2D rig;
+    Vector2 pos, playerPos, dir;
+    Player play;
+    [SerializeField] float Speed;
+    void Start(){
+        rig = GetComponent<Rigidbody2D>();
+        play = FindObjectOfType<Player>();
+        Speed = 0.03f;
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update(){
+        pos = rig.position;
+        playerPos = play.GetComponent<Rigidbody2D>().position;
+        dir = playerPos - pos;
+        dir.Normalize();
+        pos += dir * Speed;
+        rig.MovePosition(pos);
+    }
+}

76e8599e661c350f54af89bdad0202b3cfbe4cd0
RaulBRT <raulbrto@gmail.com>
2021-04-12 15:09:01 -0300

    Save Game funciona

---
+++ Assets/Actors/Fases/Scripts/MainHall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainHall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerStats.LoadGame();
+    }
+}
